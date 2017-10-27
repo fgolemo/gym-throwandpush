d8124aa1d5168f2e4feded1ef2f585d8118b4a36
Florian Golemo <fgolemo@gmail.com>
2017-10-27 13:18:24 -0400

    init cheetah env

---
+++ gym_throwandpush/envs/cheetah2.py
@@ -0,0 +1,143 @@
+import gym
+import numpy as np
+import time
+from gym import utils, error, spaces
+
+from gym_throwandpush.envs import MujocoEnvPusher2
+from gym_throwandpush.envs.mujoco_env_cheetah import MujocoEnvCheetah2
+
+NOT_INITIALIZED_ERR = "Before doing a reset or your first " \
+                      "step in the environment " \
+                      "please call env._init()."
+
+try:
+    import mujoco_py
+    from mujoco_py.mjlib import mjlib
+except ImportError as e:
+    raise error.DependencyNotInstalled(
+        "{}. (HINT: you need to install mujoco_py, and also perform the setup instructions here: https://github.com/openai/mujoco-py/.)".format(
+            e))
+
+
+class Cheetah2Env(MujocoEnvCheetah2, utils.EzPickle):
+    isInitialized = False
+
+    def _init(self, torques={}, colored=True):
+        params = {
+            "torques": torques,
+        }
+        self.isInitialized = True
+        utils.EzPickle.__init__(self)
+        xml = 'half_cheetah'
+        if colored:
+            xml += "-colored"
+
+        MujocoEnvCheetah2.__init__(self, xml + '.xml', 5, params)
+
+    def __init__(self):
+        self.metadata = {
+            'render.modes': ['human', 'rgb_array'],
+            'video.frames_per_second': 50
+        }
+        self.obs_dim = 17
+        self.act_dim = 6
+
+        self.action_space = spaces.Box(
+            -np.ones(self.act_dim),
+            np.ones(self.act_dim)
+        )
+
+        high = np.inf * np.ones(self.obs_dim)
+        low = -high
+        self.observation_space = spaces.Box(low, high)
+
+        self._seed()
+
+
+    def _step(self, action):
+        xposbefore = self.model.data.qpos[0, 0]
+        self.do_simulation(action, self.frame_skip)
+        xposafter = self.model.data.qpos[0, 0]
+        ob = self._get_obs()
+        reward_ctrl = - 0.1 * np.square(action).sum()
+        reward_run = (xposafter - xposbefore)/self.dt
+        reward = reward_ctrl + reward_run
+        done = False
+        return ob, reward, done, dict(reward_run=reward_run, reward_ctrl=reward_ctrl)
+
+    def _get_obs(self):
+        return np.concatenate([
+            self.model.data.qpos.flat[1:],
+            self.model.data.qvel.flat,
+        ])
+
+    def reset_model(self):
+        qpos = self.init_qpos + self.np_random.uniform(low=-.1, high=.1, size=self.model.nq)
+        qvel = self.init_qvel + self.np_random.randn(self.model.nv) * .1
+        self.set_state(qpos, qvel)
+        return self._get_obs()
+
+    def viewer_setup(self):
+        self.viewer.cam.distance = self.model.stat.extent * 0.5
+
+
+if __name__ == '__main__':
+    import gym_throwandpush
+
+    env = gym.make("HalfCheetah-v1")
+
+    print (env.action_space.high, env.action_space.low)
+
+    env = gym.make("HalfCheetah2-v0")
+
+    print (env.action_space.high, env.action_space.low)
+    env.env._init(
+        torques={
+            "bthigh": 120,
+            "bshin": 90,
+            "bfoot": 60,
+            "fthigh": 120,
+            "fshin": 60,
+            "ffoot": 30
+
+            # "bthigh": 240,
+            # "bshin": 45,
+            # "bfoot": 120,
+            # "fthigh": 60,
+            # "fshin": 120,
+            # "ffoot": 30
+
+            # "bthigh": 1200,
+            # "bshin": 9,
+            # "bfoot": 600,
+            # "fthigh": 12,
+            # "fshin": 600,
+            # "ffoot": 3
+        },
+        colored=True,
+    )
+    env.reset()
+
+
+    # def split_obs(obs):
+    #     qpos = obs[:7]  # robot has 7 DOF, so 7 angular positions
+    #     qvel = obs[7:14]  # 7 angular velocities
+    #     # these two above vectors are what's interesting for sim+
+    #
+    #     tip_pos = obs[14:17]  # 1 tip position in 3D space
+    #     obj_pos = obs[17:20]  # 1 object position in 3D space
+    #     gol_pos = obs[20:23]  # 1 goal position in 3D space
+    #     return (qpos, qvel, tip_pos, obj_pos, gol_pos)
+
+
+    for i in range(1000):
+        env.render()
+        action = env.action_space.sample()
+        # print(action.shape)
+        obs, reward, done, misc = env.step(action)
+        # print (obs.shape)
+        # quit()
+
+        # obs_tup = split_obs(np.around(obs, 3))
+        # print(obs_tup)
+        time.sleep(.03)
---
+++ gym_throwandpush/envs/mujoco_env_cheetah.py
@@ -0,0 +1,104 @@
+import datetime
+import os
+import tempfile
+import xml.etree.cElementTree as ET
+
+import numpy as np
+from gym import error
+from gym.envs.mujoco import MujocoEnv
+
+try:
+    import mujoco_py
+    from mujoco_py.mjlib import mjlib
+except ImportError as e:
+    raise error.DependencyNotInstalled(
+        "{}. (HINT: you need to install mujoco_py, and also perform the setup instructions here: https://github.com/openai/mujoco-py/.)".format(
+            e))
+
+
+class MujocoEnvCheetah2(MujocoEnv):
+    def __init__(self, model_path, frame_skip, model_parameters):
+        # assert "lengths" in model_parameters
+        # assert len(model_parameters["lengths"]) == 7
+        assert "torques" in model_parameters
+        assert len(model_parameters["torques"]) == 6
+        # assert "fov" in model_parameters
+        # assert "colors" in model_parameters
+
+        if model_path.startswith("/"):
+            fullpath = model_path
+        else:
+            fullpath = os.path.join(os.path.dirname(__file__), "assets", model_path)
+        if not os.path.exists(fullpath):
+            raise IOError("File %s does not exist" % fullpath)
+
+        modified_xml_path = self._modifyXml(fullpath, model_parameters)
+        # print('new xml path: {}'.format(modified_xml_path))
+        self.model = mujoco_py.MjModel(modified_xml_path)
+
+        self.frame_skip = frame_skip
+        self.data = self.model.data
+        self.viewer = None
+
+        self.init_qpos = self.model.data.qpos.ravel().copy()
+        self.init_qvel = self.model.data.qvel.ravel().copy()
+
+    def _modifyXml(self, xml_file, model_parameters):
+
+        tree = ET.ElementTree(file=xml_file)
+        root = tree.getroot()
+
+        # for i in range(2):
+        #     bodies = "/".join(["body"] * (i + 1))
+        #
+        #     arm_value = model_parameters["arm{}".format(i)]
+        #
+        #     arm = root.find('worldbody/{}/geom'.format(bodies))
+        #     arm.set('fromto', '0 0 0 {} 0 0'.format(arm_value))
+        #
+        #     if "arm" + str(i) in model_parameters["colors"]:
+        #         arm.set('rgba', '{} 1'.format(model_parameters["colors"]["arm" + str(i)]))
+        #
+        #     body = root.find('worldbody/{}/body'.format(bodies))
+        #     body.set('pos', '{} 0 0'.format(arm_value))
+
+        for joint_name,torque_val in model_parameters["torques"].items():
+            joint = root.find('actuator/motor[@joint="{}"]'.format(joint_name))
+            joint.set('gear', str(float(torque_val)))
+
+        # if "arenaBackground" in model_parameters["colors"]:
+        #     arena = root.find('worldbody/geom[@type="plane"]')
+        #     arena.set('rgba', '{} 1'.format(model_parameters["colors"]["arenaBackground"]))
+        #
+        # if "arenaBorders" in model_parameters["colors"]:
+        #     arenaBorders = root.findall('worldbody/geom[@type="capsule"]')
+        #     for arenaBorder in arenaBorders:
+        #         arenaBorder.set('rgba', '{} 1'.format(model_parameters["colors"]["arenaBorders"]))
+
+        # fovy = root.find('visual/global')
+        # fovy.set('fovy', str(model_parameters["fov"]))
+
+        file_name = os.path.basename(xml_file)
+        tmp_dir = tempfile.gettempdir()
+        now = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+        file_name_with_date = "{}-{}".format(now, file_name)
+
+        new_file_path = os.path.join(tmp_dir, file_name_with_date)
+
+        tree.write(new_file_path, "UTF-8")
+
+        return new_file_path
+
+    def _render(self, mode='human', close=False):
+        if close:
+            if self.viewer is not None:
+                self._get_viewer().finish()
+                self.viewer = None
+            return
+
+        if mode == 'rgb_array':
+            self._get_viewer().render()
+            data, width, height = self._get_viewer().get_image()
+            return np.fromstring(data, dtype='uint8').reshape(height, width, 3)[::-1, :, :]
+        elif mode == 'human':
+            self._get_viewer().loop_once()
