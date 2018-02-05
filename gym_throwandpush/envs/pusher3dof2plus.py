10216bb3a1045514448a4e14ee50ee97c118a1e5
Adrien Ali Taiga <adrien.alitaiga@gmail.com>
2018-02-05 17:01:01 -0500

    working pusher3dof2plus

--- gym_throwandpush/envs/pusher3dof2plus.py
+++ gym_throwandpush/envs/pusher3dof2plus.py
@@ -5,7 +5,7 @@ from torch import from_numpy, load
 from torch.autograd import Variable
 
 
-class Pusher3DofInferenceWrapper(gym.ObservationWrapper):
+class Pusher3DofInferenceWrapper(gym.Wrapper):
     def __init__(self, env):
         super(Pusher3DofInferenceWrapper, self).__init__(env)
         self.env = env
@@ -14,18 +14,21 @@ class Pusher3DofInferenceWrapper(gym.ObservationWrapper):
         self.net = net
         checkpoint = load(modelPath, map_location=lambda storage, loc: storage)
         self.net.load_state_dict(checkpoint['state_dict'])
-        self.net.cuda()
+        self.net = self.net.cpu()
         print("DBG: MODEL LOADED:",modelPath)
 
-    def _observation(self, observation):
-        obs = self.env.env._get_obs()
-        variable = Variable(from_numpy(obs[None,None,:-4]).cuda().float(), volatile=True, requires_grad=False)
-        obs_plus = self.net.forward(variable)
+    def _step(self, action):
+        obs_pre_action = self.env.env._get_obs()
+        obs_sim, rew, done, info = self.env.step(action)
+        input_ = np.concatenate([obs_pre_action[None,None,:6], action[None,None,:], obs_sim[None,None,:6]], axis=2)
+        variable = Variable(from_numpy(input_).float(), volatile=True)
+        obs_real = self.net.forward(variable)
 
-        self._set_to_simplus(obs, obs_plus.data.cpu().numpy()[0,0,:])
-        return self.env.env._get_obs()
+        # Udpate the environnement with the new state
+        self._set_to_simplus(obs_real.data.cpu().numpy()[0,0,:])
+        return self.env.env._get_obs(), rew, done, info
 
-    def _set_to_simplus(self, origin, correction):
+    def _set_to_simplus(self, correction):
         qpos = self.env.env.model.data.qpos.ravel().copy()
         qvel = self.env.env.model.data.qvel.ravel().copy()
 
@@ -38,8 +41,7 @@ class Pusher3DofInferenceWrapper(gym.ObservationWrapper):
         self.net.zero_hidden()  # !important
         self.net.hidden[0].detach_()  # !important
         self.net.hidden[1].detach_()  # !important
-        # print("DBG: HIDDEN STATE RESET AND DETACHED")
-        return super(Pusher3DofInferenceWrapper, self)._reset()
+        return self.env.reset()
 
 def Pusher3Dof2Plus(base_env_id):
     return Pusher3DofInferenceWrapper(gym.make(base_env_id))
